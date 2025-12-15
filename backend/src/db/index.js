94884880dc4415df3b68f159bddee4d37f58f47c
Aryan <aryankawale163@gmail.com>
2025-12-15 22:54:44 +0530

    feat: Establish core backend structure with user authentication, routes, and API utilities.

---
+++ backend/src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import { DB_NAME } from "../utils/constants.js"
+dotenv.config()
+
+export const dbConnect = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URL.replace(/\/$/, "")}/${DB_NAME}`)
+        console.log(`\n MongoDB connected: !! DB HOST : ${connectionInstance.connection.host}`)
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
